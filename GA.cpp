183745801bd2f0ee6bd93d4ebcfc199e61858ef8
AlbertYoung <1004018919@qq.com>
2019-04-28 11:10:44 +0800

    Change the interface for the GA algorithm.

--- GA.cpp
+++ GA.cpp
@@ -1,13 +1,2 @@
 #include "GA.h"
 
-GA::GA(uint32_t num, uint32_t *initGene):_num(num) {
-    _fitness = new uint32_t[num];
-    if(initGene == nullptr) {
-        _gene = new uint32_t[num];
-        _generateInitGene();
-    } else {
-        _gene = initGene;
-        std::copy(initGene, initGene + num, _gene);
-    }
-}
-
--- GA.h
+++ GA.h
@@ -7,22 +7,12 @@
 class GA {
 
 private:
-    uint32_t* _gene;
-    uint32_t* _fitness;
-    uint32_t _num;
-
     virtual void _generateInitGene() = 0;
     virtual void _fitnessCal() = 0;
     virtual void _cross() = 0;
     virtual void _mutation() = 0;
 
 public:
-    explicit GA(uint32_t num, uint32_t *initGene = nullptr);
-    ~GA() {
-        delete [] _gene;
-        delete [] _fitness;
-    }
-
     /*
      * Execute the GA algorithm.
      * Return the best gene and the best fitness through the
