1c6ab369cab13b42f99c8ac07f4c214877264e7b
AlbertYoung <1004018919@qq.com>
2019-05-24 21:06:31 +0800

    Add interfaces. Add the mutation, cross rate. Add  private members.

--- Include/SchGA.h
+++ Include/SchGA.h
@@ -35,9 +35,11 @@ public:
 
     void evaluate(uint32_t iterations,
                   uint32_t &bestGene, double_t &bestFitness) override;
-    SchGA(uint32_t population, FLIGHT_STATE* flights, TASK* taskTable, double_t rho);
+    SchGA(uint32_t population, FLIGHT_STATE* flights, TASK* taskTable, double_t rho, double_t crossRate, double_t mutationRate);
     ~SchGA();
     void mutationTest();
+    const uint32_t *getBestGene();
+    uint32_t getGeneLength();
 
 private:
     TASK_PARAMETER* _visitTask(uint32_t task);
@@ -51,6 +53,7 @@ private:
     void _fitnessCal() override;
     void _cross(uint32_t parentA, uint32_t parentB, uint32_t &childA, uint32_t &childB) override;
     uint32_t _mutation(uint32_t child) override;
+    void _selectParents(uint32_t* parentsNo, uint32_t num);
 
     TASK *_taskTable;
     FLIGHT_STATE *_initialFlightState;
@@ -58,7 +61,12 @@ private:
     uint32_t *_gene;
     uint32_t *_nextGene;
     uint32_t _geneLength;
-    uint32_t _bestGene;
+    uint32_t* _bestGene;
+    uint32_t* _worstGene;
+    double_t _bestFitness;
+    uint32_t _feasibleGeneCnt;
+    uint32_t _crossRate;
+    uint32_t _mutationRate;
     double_t *_fitness;
     double_t _rho;
     std::mt19937 _rng;
@@ -67,7 +75,7 @@ private:
     uint32_t _numPath;
 };
 
-SchGA::TASK_PARAMETER* SchGA::_visitTask(uint32_t task) {
+inline SchGA::TASK_PARAMETER* SchGA::_visitTask(uint32_t task) {
     if(task > _taskTable->totalNum) {
         return nullptr;
     }
@@ -77,6 +85,14 @@ SchGA::TASK_PARAMETER* SchGA::_visitTask(uint32_t task) {
         pQueue++;
     }
     return (pQueue->tasks + task);
+}
+
+inline const uint32_t* SchGA::getBestGene() {
+    return const_cast<const uint32_t*>(_bestGene);
+}
+
+inline uint32_t SchGA::getGeneLength() {
+    return _geneLength;
 }
 
 #endif //GAHW_SCHGA_H
